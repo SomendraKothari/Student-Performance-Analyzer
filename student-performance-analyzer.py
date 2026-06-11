732888b73f1cedf61883dc9b8713014dff175456
SomendraKothari <ch25btech11041@iith.ac.in>
2026-06-11 15:14:15 +0530

    final

--- student-performance-analyzer.py
+++ student-performance-analyzer.py
@@ -6,12 +6,15 @@ data_frame = pd.read_csv("E:/coding/DATASETS/Student Performance/marksheet.csv")
 total_marks=data_frame.loc[:,['Science','English','Maths','History']].apply(sum,axis=1)
 # Check for index having maximum marks i.e., topper's marks and store its id and name
 topper = data_frame.loc[total_marks.idxmax(),['id','Name']]
+print(f"Topper : {topper.id}    {topper.Name}")
 # Calculate the class average
 cls_avg = total_marks.sum()/len(data_frame)
+print(f"Class Average : {cls_avg}")
 # For students who have failed in one or more subjects
 fail = data_frame.loc[(data_frame.Science<33) | (data_frame.English<33) | (data_frame.Maths<33) | (data_frame.History<33),['id']]
 # Check for students having total marks more than 300 i.e., having a percentage >=75%
-morethan75 = data_frame.loc[total_marks>=300,['id','Name']]
+morethan75 = data_frame.loc[total_marks>=300,['id','Name']].reset_index(drop=True)
+print(f"Students with more than 75 Percentage :\n {morethan75}")
 # Create a Series having Percentage of all students i.e., Results
 rs = total_marks.map(lambda x:str(round(x/4,2))+'%')
 # Failing students results
@@ -20,16 +23,13 @@ rs[fail.index] = 'Fail'
 data_frame['Result'] = rs
 # Create a new DataFrame for results and upload it into a csv
 results = data_frame.loc[:,['id','Name','Result']]
-print(f"Topper : {topper.id}    {topper.Name}")
-print(f"Class Average : {cls_avg}")
-print(f"Students with more than 75 Percentage :\n {morethan75.Name}")
 # For plotting graph
 top25 = results.iloc[:25,2]
 top25=pd.to_numeric(top25.str.replace("%","",regex=False),errors='coerce')
-graph = top25.plot(kind='bar')
-plt.xlabel('Id')
-plt.ylabel('Percentage')
-plt.title('First 25 Student Performance')
-plt.xticks(rotation=360)
-plt.show()
-results.to_csv("RESULTS",index=False)
+# graph = top25.plot(kind='bar')
+# plt.xlabel('Id')
+# plt.ylabel('Percentage')
+# plt.title('First 25 Student Performance')
+# plt.xticks(rotation=360)
+# plt.show()
+# results.to_csv("RESULTS",index=False)
